ed046bd034a9a1b1f5a1dd5dbe0f82503cdae5e3
Eliot Nerrand <121872712+GasterSans17138@users.noreply.github.com>
2026-04-06 17:26:25 +0200

    Remove hardcoded AI controller assignment to reduce merge conflicts

--- Source/PSP/Variant_Shooter/AI/ShooterAICharacter.cpp
+++ Source/PSP/Variant_Shooter/AI/ShooterAICharacter.cpp
@@ -1,12 +1,10 @@
 #include "ShooterAICharacter.h"
 #include "ShooterSquadComponent.h"
-#include "ShooterAIController.h"
 
 AShooterAICharacter::AShooterAICharacter()
 {
 	SquadComponent = CreateDefaultSubobject<UShooterSquadComponent>(TEXT("SquadComponent"));
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
-	AIControllerClass = AShooterAIController::StaticClass();
 }
 
 void AShooterAICharacter::RefreshSquadOrder()
